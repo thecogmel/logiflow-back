595d94a2c140d1fbaece35b8be5cb09434968f4b
Erick Medeiros <erick.medeiros@4mglobaltec.com.br>
2024-05-28 10:31:44 -0300

    feat: :sparkles: Add Delivery model and controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,11 +10,13 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Public } from 'src/auth/decorator/public.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Public()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
